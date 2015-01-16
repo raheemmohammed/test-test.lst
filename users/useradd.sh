59c343dcbc7add427199a45b227f32611bcb7af4
Raheem Mohammed <mail@raheem.me>
2015-01-16 18:09:32 +0000

    h
    h

--- users/useradd.sh
+++ users/useradd.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-GOOD_USERS="./goodusers.lst" 
-BAD_USERS="./badusers.lst"
+GOOD_USERS="./goodusers.lst"
 HOME_DIR="/home/"
 
 cat ${GOOD_USERS} | \
 while read USER
 do
 	useradd -m -d ${HOME_DIR}${USER} ${USER}
-
-cat ${BAD_USERS} | \
-while read USER
-do
-	userdel -r ${USER}
 done
